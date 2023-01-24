bdc08f10b70e3e929581f8f02078bcd67fd19f90
chp320 <chp320@naver.com>
2023-01-24 09:00:37 +0900

    adapter에서 프라이빗/퍼블릭 룸 확인

--- src/server.js
+++ src/server.js
@@ -14,8 +14,36 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const sids = wsServer.sockets.adapter.sids;
+    const rooms = wsServer.sockets.adapter.rooms;
+    /*
+     * socket.io에서는 '개별 소켓'은 서버에서 제공하는 개인공간을 할당받고 그 곳에 들어간다. 즉, 소켓과 서버 사이에 형성된 채팅룸. (private room). 소켓id는 private room 의 id와 동일!
+     * 이후 join 메서드를 사용해서 다른 소켓과 그룹을 형성해 채팅룸을 만듦. (public room)
+     * adapter 에서 rooms, sids 를 확인할 수 있고.. 아래와 같이 private, public 룸을 확인 가능!
+       - private room: sids 내 속성이 rooms 에도 존재한다!
+       - public room: sids 내 속성이 rooms 에 없다!!
+     */
+    const publicRooms = [];
+    rooms.forEach((value, key) => {
+        if(sids.get(key) === undefined) {
+            publicRooms.push(key)
+        }
+    })
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anonymous";   // 닉네임 입력되기 전까지 '익명'으로 표기
+    /*
+        서버와 통신을 맺고 있는 소켓 어플리케이션은 '어댑터'라는 객체를 통해 관리되고 있다.
+        어댑터를 통해 연결되어 있는 소켓 어플리케이션과 채팅룸 수를 확인한다. -> onAny() 메서드 사용!
+        * onAny() : 소켓에서 발생하는 모든 이벤트에 대응하는 이벤트 핸들러 등록 메서드.
+     */
+    socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter);
+        console.log(`Socket Event: ${event}`);
+    });
     socket.on("enter_room", (roomName, done) => {
         done();     // front에서 전달된 콜백함수(done)를 호출한다!
         socket.join(roomName);
